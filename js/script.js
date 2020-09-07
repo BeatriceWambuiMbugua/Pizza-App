ad53f8b6ed5524202c33ebf21316bb541caf24c8
Moringa School Access <wambuimbeatrice@gmail.com>
2020-09-08 00:35:27 +0300

    inputting the topping prices

--- js/script.js
+++ js/script.js
@@ -254,8 +254,119 @@ $(function(){
                                 }
                                 break;
                         }
-                    
-
+                        break;
+                        case flavour = "vegfeast":
+                        switch (size){
+                            case size = "regular":
+                                price = 600;
+                                if (crust === "thin") {
+                                    totalPrice = (price * number) + 150;
+                                } else if (crust === "thick") {
+                                    totalPrice = (price * number) + 250;
+                                } else if (crust === "flatbread") {
+                                    totalPrice = (price * number) + 280;
+                                } else {
+                                    totalPrice = (price * number) + 280;
+                                }
+                                break;
+                                case size = "medium":
+                                    price = 900;
+                                    if (crust === "thin") {
+                                        totalPrice = (price * number) + 150;
+                                    } else if (crust === "thick") {
+                                        totalPrice = (price * number) + 250;
+                                    } else if (crust === "flatbread") {
+                                        totalPrice = (price * number) + 280;
+                                    } else {
+                                        totalPrice = (price * number) + 280;
+                                    }
+                                    break;
+                                case size = "large":
+                                price = 1200;
+                                if (crust === "thin") {
+                                    totalPrice = (price * number) + 150;
+                                } else if (crust === "thick") {
+                                    totalPrice = (price * number) + 250;
+                                } else if (crust === "flatbread") {
+                                    totalPrice = (price * number) + 280;
+                                } else {
+                                    totalPrice = (price * number) + 280;
+                                }
+                                break;
+                        }
+                        break;
+                        case flavour = "chickenmacon":
+                            switch (size){
+                                case size = "regular":
+                                    price = 600;
+                                    if (crust === "thin") {
+                                        totalPrice = (price * number) + 150;
+                                    } else if (crust === "thick") {
+                                        totalPrice = (price * number) + 250;
+                                    } else if (crust === "flatbread") {
+                                        totalPrice = (price * number) + 280;
+                                    } else {
+                                        totalPrice = (price * number) + 280;
+                                    }
+                                    break;
+                                    case size = "medium":
+                                        price = 900;
+                                        if (crust === "thin") {
+                                            totalPrice = (price * number) + 150;
+                                        } else if (crust === "thick") {
+                                            totalPrice = (price * number) + 250;
+                                        } else if (crust === "flatbread") {
+                                            totalPrice = (price * number) + 280;
+                                        } else {
+                                            totalPrice = (price * number) + 280;
+                                        }
+                                        break;
+                                    case size = "large":
+                                    price = 1200;
+                                    if (crust === "thin") {
+                                        totalPrice = (price * number) + 150;
+                                    } else if (crust === "thick") {
+                                        totalPrice = (price * number) + 250;
+                                    } else if (crust === "flatbread") {
+                                        totalPrice = (price * number) + 280;
+                                    } else {
+                                        totalPrice = (price * number) + 280;
+                                    }
+                                    break;
+                            }
+                            break;
+        };
+        switch (topping){
+            case topping = "tomato":
+                totalPrice = totalPrice + 80;
+                break;
+            case topping = "onions":
+                totalPrice = totalPrice + 80;
+                break;
+            case topping = "veggiesalad":
+                totalPrice = totalPrice + 80;
+                break;
+            case topping = "greenpepper":
+                totalPrice = totalPrice + 80;
+                break;
+            case topping = "olives":
+                totalPrice = totalPrice + 120;
+                break;
+            case topping = "pineapple":
+                totalPrice = totalPrice + 120;
+                break;
+            case topping = "macon":
+                totalPrice = totalPrice + 120;
+                break;
+            case topping = "mince":
+                totalPrice = totalPrice + 120;
+                break;
+            case topping = "beef":
+                totalPrice = totalPrice + 180;
+                break;
+            case topping = "chicken":
+                totalPrice = totalPrice + 180;
+                break;
         };
 
 
