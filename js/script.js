883246871fd6a429e8a077d0006657152aa84608
Moringa School Access <wambuimbeatrice@gmail.com>
2020-09-08 01:14:35 +0300

    activating the delivery code

--- js/script.js
+++ js/script.js
@@ -383,5 +383,14 @@ $(function(){
         + newOrder.t + "<br>" + "Number of pizza's : "
         + newOrder.n + "<br>" + "Total Price : "
         + newOrder.total + "<br><br>").css('font-family', 'system-ui').css('font-size', '20px');
+
+        $(".deliver").click(function(){
+            $('.summary').slideUp();
+            $('#list').slideUp();
+            $('.summary').text("Provide location details").slideDown();
+            $('.deliver').hide(1000);
+            $('.delivernot').hide(1000);
+            $('.cdata-overlay').slideDown();
+        });
     });
 });
